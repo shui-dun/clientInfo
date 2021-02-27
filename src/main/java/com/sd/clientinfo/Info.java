10ee22bbda5beb4898ac8d42957a61d27e34df6d
shui-dun <2604697844@qq.com>
2021-02-27 13:02:38 +0800

    update

---
+++ src/main/java/com/sd/clientinfo/Info.java
@@ -0,0 +1,54 @@
+package com.sd.clientinfo;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.apache.commons.io.IOUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+import java.util.Enumeration;
+import java.util.HashMap;
+
+@WebServlet("/info")
+public class Info extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Gson gson = new GsonBuilder().setPrettyPrinting().disableHtmlEscaping().create();
+        InfoBean bean = new InfoBean();
+        bean.method = req.getMethod();
+        bean.requestURL = req.getRequestURL().toString();
+        bean.requestURI = req.getRequestURI();
+        bean.queryString = req.getQueryString();
+        bean.remoteHost = req.getRemoteHost();
+        bean.remoteAddr = req.getRemoteAddr();
+        bean.remotePort = req.getRemotePort();
+        bean.localAddr = req.getLocalAddr();
+        bean.localName = req.getLocalName();
+        bean.localPort = req.getLocalPort();
+        bean.headers = new HashMap<>();
+        Enumeration<String> headerNames = req.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String headerName = headerNames.nextElement();
+            bean.headers.put(headerName, req.getHeader(headerName));
+        }
+        bean.body = IOUtils.toString(req.getInputStream(), Charset.defaultCharset());
+        resp.getWriter().write(gson.toJson(bean));
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req, resp);
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req, resp);
+    }
+}
---
+++ src/main/java/com/sd/clientinfo/InfoBean.java
@@ -0,0 +1,31 @@
+package com.sd.clientinfo;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class InfoBean {
+    public String method;
+
+    public String requestURL;
+
+    public String requestURI;
+
+    public String queryString;
+
+    public String remoteHost;
+
+    public String remoteAddr;
+
+    public int remotePort;
+
+    public String localAddr;
+
+    public String localName;
+
+    public int localPort;
+
+    public Map<String, String> headers;
+
+    public String body;
+
+}
